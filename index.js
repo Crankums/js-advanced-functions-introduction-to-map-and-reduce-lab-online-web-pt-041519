c973dd292f72e464b21421cecf23d8d73bc03e20
Craig Donavin <crdonavin@gmail.com>
2019-11-23 03:23:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,5 +28,9 @@ function mapToSquare(src) {
 }
 
 function reduceToTotal(src, startingPoint=0){
-
+  let total = startingPoint
+  for (let i=0; i<src.length; i++) {
+    total += i
+  }
+  return total
 }
